65b75a2c4fec4d31dc9eb6ffb6765be0215678ce
izah2587 <izahs2003@gmail.com>
2024-06-06 05:32:51 +0400

    hix voting files

--- core/models.py
+++ core/models.py
@@ -1,36 +1,39 @@
 from django.db import models
 
+#Member of European Parlament
 class MEP(models.Model):
     unique_identifier = models.IntegerField()
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+
     date_of_birth = models.DateField(null=True)
     gender = models.CharField(max_length=6, null=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.id
 
-# Meeting model to store meeting data
-class Meeting(models.Model):
-    activity_id = models.CharField(max_length=255, unique=True)
-    activity_type = models.CharField(max_length=255)
 
 class Vote(models.Model):
     unique_identifier = models.CharField(max_length=250)
+    label = models.CharField(max_length=250)
+
     outcome = models.CharField(max_length=50)
+    
     number_of_attendees = models.IntegerField()
     number_of_favor = models.IntegerField()
     number_of_against = models.IntegerField()
     number_of_abstention = models.IntegerField()
+    number_of_not_voted = models.IntegerField()
 
     def __str__(self):
-        return self.label
+        return self.id
 
 
-class MEPVoteMapping(models.Model):
+class MEP_Vote_Mapping(models.Model):
     mep = models.ForeignKey(MEP, on_delete=models.CASCADE)
     vote = models.ForeignKey(Vote, on_delete=models.CASCADE)
-    vote_type = models.CharField(max_length=50)  # 'favor', 'against', 'abstention', 'no_vote'
+    vote_type = models.CharField(max_length=20,  null = True)  # 'favor', 'against', 'did not vote' or 'abstention'
 
-    def __str__(self):
-        return f"{self.mep} - {self.vote} ({self.vote_type})"
+    class Meta:
+        unique_together = ('mep', 'vote', 'vote_type')
