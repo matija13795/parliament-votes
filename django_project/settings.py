a8d12e0f380e5a2e9d80e938dae8197cc0253012
Your Name <you@example.com>
2024-07-10 09:58:57 +0200

    preparing to pull

--- django_project/settings.py
+++ django_project/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',  # Using PostgreSQL database
         'NAME': 'my_database',  # Database name
         'USER': 'postgres',  # Database user
-        'PASSWORD': 'izahaha#567',  # Database password
+        'PASSWORD': 'password',  # Database password
         'HOST': 'localhost',  # Database host
         'PORT': '5432',  # Database port
     }
